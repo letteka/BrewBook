71af7fe8354c85c27e92e1f35b29f5021fb7e8ad
Andrew Kettel <akettel@parthenonsoftware.com>
2015-04-02 16:32:22 -0700

    initial checkin with project file

---
+++ BrewBook/BrewBook/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  BrewBook
+//
+//  Created by Andrew Kettel on 4/2/15.
+//  Copyright (c) 2015 ParthenonSoftwareGroup. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
